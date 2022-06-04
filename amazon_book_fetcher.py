e33ae3142e6d1129076964443854906a969920d2
Jinan47 <jinanmswi47@gmail.com>
2022-06-04 18:23:44 +0300

    Added test case for amazon book fetcher

--- amazon_book_fetcher.py
+++ amazon_book_fetcher.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.keys import Keys
+import pdf_file_fetcher
 
-def amazon(filename = "pdf"):
+def amazon(filename = pdf_filename + 'amazon'):
     driver = driver = webdriver.Chrome(service = ChromeService(executable_path=ChromeDriverManager().install()))
     driver.get("https://www.google.com")
     
@@ -11,3 +12,6 @@ def amazon(filename = "pdf"):
     search.send_keys(filename)
     search.send_keys(Keys.RETURN)
     x =input()
+
+if __name__ == '__main__':
+    amazon()
