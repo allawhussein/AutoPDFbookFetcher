b17432e3ac1e632aa43b68bb06db84bb143dd015
allawhussein <allawhussein@gmail.com>
2022-06-04 15:18:15 +0300

    created library_add_author.py add_author()

---
+++ library_code/library_add_author.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+from local_credentials_and_urls import *
+
+def add_author(driver, author_name, author_description):
+    driver.get(author_url)
