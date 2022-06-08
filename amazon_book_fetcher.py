52c8ad7b6cf6441abd8a77f027985ef998dc06f1
Jinan47 <jinanmswi47@gmail.com>
2022-06-08 23:41:10 +0300

    + or space

--- amazon_book_fetcher.py
+++ amazon_book_fetcher.py
@@ -7,18 +7,19 @@ import pdf_file_fetcher
 
 
 def amazon(filename):
+    
     if '.pdf' in filename:
-        final_fn = filename.replace('.pdf', '')
+        final_fn = filename.replace('.pdf', '+')
+
     driver = driver = webdriver.Chrome(service = ChromeService(executable_path=ChromeDriverManager().install()))
     driver.get("https://www.google.com/search?q=" + final_fn + '&tbm=bks')
-
     x =input()
 
 if __name__ == '__main__':
-    #this source, destination are for Hussein Allaw
-    src = "C:\\Users\\Hussein\\Desktop\\Microbiology\\"
-    dest = "C:\\Users\\Hussein\\Desktop\\Microbiology\\DONE-MicroBiology\\"
-    dest, pdf_filename = pdf_file_fetcher.move_pdf(src, dest)
+    # #this source, destination are for Hussein Allaw
+    # src = "C:\\Users\\Hussein\\Desktop\\Microbiology\\"
+    # dest = "C:\\Users\\Hussein\\Desktop\\Microbiology\\DONE-MicroBiology\\"
+    # dest, pdf_filename = pdf_file_fetcher.move_pdf(source, dest)
     #this source, destination is for S. Jinan Al-Mswi
-    #dest, pdf_filename = pdf_file_fetcher.move_pdf()
+    dest, pdf_filename = pdf_file_fetcher.move_pdf()
     amazon(pdf_filename)
