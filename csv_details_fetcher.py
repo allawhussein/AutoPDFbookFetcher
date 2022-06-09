2a5aef0bc52f77b6be39538d631eb2e8b18ddd40
Jinan47 <jinanmswi47@gmail.com>
2022-06-09 13:10:18 +0300

    try it

--- csv_details_fetcher.py
+++ csv_details_fetcher.py
@@ -1,21 +1,41 @@
-import os
+# import os
 
-def csv_book_details(book_name, book_author, book_path):#this is what I mean by argument, and passing stuff through it
-    if os.path.exists(book_path):
-        print('Loading')
-        f = open(book_path, "a")  
-        f.write('Test1 pass')
-        f.close()
+# def csv_book_details(book_name, book_author, book_path, csv_path):#this is what I mean by argument, and passing stuff through it
+#     if not os.path.exists(csv_path):
 
-    if not os.path.exists(book_path):
+#         exist_ok=False
+
+csv_path = 'C:\\Users\\LenovoPc\\Desktop\\CSV Book_Details\\t.txt'
+
+#         os.makedirs(csv_path)
+
+#         f = open(csv_path, "a")   
+
+#         f.write('Book name: ' + book_name)
+#         f.write('\n')
+
+#         f.write('Author name:', book_author)
+#         f.write('\n')
+
+#         f.write('Book path:', book_path)
+#         f.write('\n')
+
+#         f.close()
+
+
+#     if os.path.exists(csv_path):
+f = open(csv_path, "r") 
+f.readline(1)
+
+
+whattoReturn = "None"
+strToFind = 'w'
+if strToFind in f:
+    whattoReturn = strToFind
+    print(whattoReturn)
+    f.close() 
+    
         
-        # print('pass')             
-        os.makedirs(book_path)
-        
-        print('pass')
-        # f = open(book_path, "w") 
-        # f.write('Test3 pass')
-        # f.close() 
 
-if __name__ == "__main__":
-    csv_book_details('BOOK NAME', 'BOOK AUTHOR', 'PATH')#here is a test case, we'll replace these arbitrary values with something useful later on, but the main usage is for anther script, that will pass these details.
+# if __name__ == "__main__":
+#     csv_book_details('BOOK NAME', 'BOOK AUTHOR', 'PATH') # here is a test case, we'll replace these arbitrary values with something useful later on, but the main usage is for anther script, that will pass these details.
