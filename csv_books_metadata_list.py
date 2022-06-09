a93d3d2622cc68e4c5f5211068d6a534a8d21f76
allawhussein <allawhussein@gmail.com>
2022-06-09 15:17:35 +0300

    added inital details for csv_books_metadata_list.py

---
+++ csv_books_metadata_list.py
@@ -0,0 +1,7 @@
+from local_books_manager import book_metadata_seeker
+from local_books_manager import csv_details_fetcher
+from local_books_manager import pdf_file_fetcher
+
+source_folder_path= "C:\\Users\\Hussein\\Desktop\\Microbiology\\"
+destination_folder_path= "C:\\Users\\Hussein\\Desktop\\Microbiology\\DONE-MicroBiology\\"
+image_folder_path= "C:\\Users\\Hussein\\Desktop\\Microbiology\\Images-Microbiolog\\"
