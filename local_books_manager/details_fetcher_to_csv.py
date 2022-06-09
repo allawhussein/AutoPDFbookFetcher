261e301cb6903b3c46e3ca0a83aca66c5c174c06
allawhussein <allawhussein@gmail.com>
2022-06-09 16:37:16 +0300

    updated_details_fetcher_to_csv.py arguments

--- local_books_manager/details_fetcher_to_csv.py
+++ local_books_manager/details_fetcher_to_csv.py
@@ -1,18 +1,18 @@
 import os
 
-def csv_book_details(book_name, book_author, book_path, book_review, image_path, csv_path):
+def csv_book_details(book_name, book_authors, book_review, publication_date, book_path, image_path, csv_path):
     if not os.path.exists(csv_path):
         with open(csv_path, 'w') as csv_file:
-            csv_file.write("BookName, BookAuthor(s), BookPATH, BookReview, ImagePATH\n")
+            csv_file.write("BookName, BookAuthor(s), BookReview, PublicatoinDate, BookPATH, ImagePATH\n")
     else:
         with open(csv_path, 'r') as csv_file:
             header= csv_file.readline()
-        if header != "BookName, BookAuthor(s), BookPATH, BookReview\n":
-            return csv_book_details(book_name, book_author, book_path, book_review, image_path, csv_path)
+        if header != "BookName, BookAuthor(s), BookReview, PublicatoinDate, BookPATH, ImagePATH\n":
+            return csv_book_details(book_name, book_authors, book_review, publication_date, book_path, image_path, csv_path)
 
     with open(csv_path, 'a') as csv_file:
-        csv_file.write(','.join([book_name, book_author, book_path, book_review, image_path]))
+        csv_file.write(','.join([book_name, book_authors, book_review, publication_date, book_path, image_path]) + "\n")
 
 
 if __name__ == "__main__":
-    csv_book_details('BOOK NAME', 'BOOK AUTHOR', 'PATH', 'CSV_PATH')
+    pass
