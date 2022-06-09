86bd661b3e1b6d5bb6e1f126b25aaa56338fcd79
allawhussein <allawhussein@gmail.com>
2022-06-09 16:23:47 +0300

    removed redundant comments

--- local_books_manager/details_fetcher_to_csv.py
+++ local_books_manager/details_fetcher_to_csv.py
@@ -1,6 +1,6 @@
 import os
 
-def csv_book_details(book_name, book_author, book_path, book_review, image_path, csv_path):#this is what I mean by argument, and passing stuff through it
+def csv_book_details(book_name, book_author, book_path, book_review, image_path, csv_path):
     if not os.path.exists(csv_path):
         with open(csv_path, 'w') as csv_file:
             csv_file.write("BookName, BookAuthor(s), BookPATH, BookReview, ImagePATH\n")
@@ -15,4 +15,4 @@ def csv_book_details(book_name, book_author, book_path, book_review, image_path,
 
 
 if __name__ == "__main__":
-    csv_book_details('BOOK NAME', 'BOOK AUTHOR', 'PATH', 'CSV_PATH') # here is a test case, we'll replace these arbitrary values with something useful later on, but the main usage is for anther script, that will pass these details.
+    csv_book_details('BOOK NAME', 'BOOK AUTHOR', 'PATH', 'CSV_PATH')
