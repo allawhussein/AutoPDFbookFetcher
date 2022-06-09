998093cb2c1faf089449c0d174eeeb7be4531869
allawhussein <allawhussein@gmail.com>
2022-06-09 16:28:28 +0300

    minor naming changes

--- pdf_cover_page_skimmer/pdf_cover_page_skimmer.py
+++ pdf_cover_page_skimmer/pdf_cover_page_skimmer.py
@@ -1,11 +1,12 @@
 from pdf2image import convert_from_path
 
 def create_cover_image(pdf_path, target_path, pdf_filename):
-    image= convert_from_path(pdf_path, first_page= 0, last_page= 1)[0]
-    image.save(target_path + pdf_filename.replace(".pdf", ".png"), "PNG")    
+    image= convert_from_path(pdf_path + pdf_filename, first_page= 0, last_page= 1)[0]
+    image.save(target_path + pdf_filename.replace(".pdf", ".png"), "PNG")
+    return target_path + pdf_filename.replace(".pdf", ".png")
 
 if __name__ == "__main__":
-    pdf_path= "C:\\Users\\Hussein\\Desktop\\MicroBiology\\Wastewater Microbiology - Gabriel Bitton.pdf"
+    pdf_path= "C:\\Users\\Hussein\\Desktop\\MicroBiology\\"
     target_path= "C:\\Users\\Hussein\\Desktop\\MicroBiology\\Images-Microbiolog\\"
     pdf_filename= "Wastewater Microbiology - Gabriel Bitton.pdf"
     create_cover_image(pdf_path, target_path, pdf_filename)
