20ba06185e141d934c3cfc1c74227507a3b0f767
allawhussein <allawhussein@gmail.com>
2022-06-09 15:17:48 +0300

    deleted csv_book_details()

--- local_books_manager/csv_details_fetcher.py
+++ local_books_manager/csv_details_fetcher.py
@@ -1,40 +1,7 @@
 import os
 
-def csv_book_details(book_name, book_author, book_path, csv_path):#this is what I mean by argument, and passing stuff through it
-    if not os.path.exists(csv_path):
-
-        exist_ok=False
-
-        
-
-        csv_path = 'C:\\Users\\LenovoPc\\Desktop\\CSV Book_Details\\'
-
-        join = os.path.join(csv_path, 'books.txt')
-
-        make = os.makedirs(join)
-
-        f = open(make, "a")   
-
-        f.write('Book name: ' + book_name)
-        f.write('\n')
-
-        f.write('Author name:', book_author)
-        f.write('\n')
-
-        f.write('Book path:', book_path)
-        f.write('\n')
-
-        f.close()
-
-
-    if os.path.exists(make):
-        with open(make) as file:
-            contents = file.read()
-            search_word = input("enter a word you want to search in file: ")
-            if search_word in contents:
-                print ('word found')
-            else:
-                print ('word not found')
+def csv_book_details(book_name, book_author, book_path, book_review, csv_path):#this is what I mean by argument, and passing stuff through it
+    
         
 
 
