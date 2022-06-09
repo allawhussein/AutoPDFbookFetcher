31facddbfd35ff620939c7289f352ea82718313d
allawhussein <allawhussein@gmail.com>
2022-06-09 10:30:51 +0300

    csv_details_fetcher Added test case, moved hardcoded arguments to function arguments

--- csv_details_fetcher.py
+++ csv_details_fetcher.py
@@ -1,12 +1,6 @@
 import os
 
-def csv_book_details():
-
-    book_name = 'BOOK NAME' + '.txt'
-    book_path = os.path.join('C:\\Users\\LenovoPc\\Desktop\\CSV Book_Details', book_name)
-
-
-
+def csv_book_details(book_name, book_author, book_path):#this is what I mean by argument, and passing stuff through it
     if os.path.exists(book_path):
         print('Loading')
         f = open(book_path, "a")  
@@ -22,3 +16,6 @@ def csv_book_details():
         # f = open(book_path, "w") 
         # f.write('Test3 pass')
         # f.close() 
+
+if __name__ == "__main__":
+    csv_book_details('BOOK NAME', 'BOOK AUTHOR', 'PATH')#here is a test case, we'll replace these arbitrary values with something useful later on, but the main usage is for anther script, that will pass these details.
