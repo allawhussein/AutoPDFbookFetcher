0322e7a38b54016a8e4231e2df2b7225ac2320cf
Jinan47 <jinanmswi47@gmail.com>
2022-06-09 10:16:14 +0300

    Help please !

--- csv_details_fetcher.py
+++ csv_details_fetcher.py
@@ -1,18 +1,24 @@
 import os
 
+def csv_book_details():
 
-book_name = 'BOOK NAME' + '.txt'
-book_path = os.path.join('C:\\Users\\LenovoPc\\Desktop\\CSV Book_Details', book_name)
-
-
-if os.path.exists(book_path):
-    print('The book', book_name, 'is founded already')
+    book_name = 'BOOK NAME' + '.txt'
+    book_path = os.path.join('C:\\Users\\LenovoPc\\Desktop\\CSV Book_Details', book_name)
 
 
-elif not os.path.exists('C:\\Users\\LenovoPc\\Desktop\\CSV Book_Details'):
-    
-    # print('pass')
 
-    os.makedirs('C:\\Users\\LenovoPc\\Desktop\\CSV Book_Details')
-     
-f = open(book_path, "a")  
+    if os.path.exists(book_path):
+        print('Loading')
+        f = open(book_path, "a")  
+        f.write('Test1 pass')
+        f.close()
+
+    if not os.path.exists(book_path):
+        
+        # print('pass')             
+        os.makedirs(book_path)
+        
+        print('pass')
+        # f = open(book_path, "w") 
+        # f.write('Test3 pass')
+        # f.close() 
