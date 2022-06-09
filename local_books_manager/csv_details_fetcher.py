e4530b6646bb088a8c33a05cf9c30387cc32dcf1
allawhussein <allawhussein@gmail.com>
2022-06-09 15:25:59 +0300

    updated csv_book_details()

--- local_books_manager/csv_details_fetcher.py
+++ local_books_manager/csv_details_fetcher.py
@@ -1,8 +1,17 @@
 import os
 
-def csv_book_details(book_name, book_author, book_path, book_review, csv_path):#this is what I mean by argument, and passing stuff through it
-    
-        
+def csv_book_details(book_name, book_author, book_path, book_review, image_path, csv_path):#this is what I mean by argument, and passing stuff through it
+    if not os.path.exists(csv_path):
+        with open(csv_path, 'w') as csv_file:
+            csv_file.write("BookName, BookAuthor(s), BookPATH, BookReview, ImagePATH\n")
+    else:
+        with open(csv_path, 'r') as csv_file:
+            header= csv_file.readline()
+        if header != "BookName, BookAuthor(s), BookPATH, BookReview\n":
+            return csv_book_details(book_name, book_author, book_path, book_review, image_path, csv_path)
+
+    with open(csv_path, 'a') as csv_file:
+        csv_file.write(','.join([book_name, book_author, book_path, book_review, image_path]))
 
 
 if __name__ == "__main__":
