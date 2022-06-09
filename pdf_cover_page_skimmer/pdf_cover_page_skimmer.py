62503a5f40b752541974e80a36a50ee18976b7ee
allawhussein <allawhussein@gmail.com>
2022-06-09 16:20:55 +0300

    optimized code for create_cover_image()

--- pdf_cover_page_skimmer/pdf_cover_page_skimmer.py
+++ pdf_cover_page_skimmer/pdf_cover_page_skimmer.py
@@ -1,13 +1,9 @@
 import os
 from pdf2image import convert_from_path
 
-def create_cover_image(pdf_path, target_path):
-    poppler_path= os.getcwd() + "\\bin"
-    pages = convert_from_path(pdf_path, dpi= 300, first_page= 0, output_folder= target_path, last_page= 1, poppler_path=poppler_path)
-    pdf_file = pdf_file[:-4]
-
-    for page in pages:
-        page.save("%s-page%d.png" % (pdf_file,pages.index(page)), "PNG")
+def create_cover_image(pdf_path, target_path, pdf_filename):
+    image= convert_from_path(pdf_path, first_page= 0, last_page= 1)[0]
+    image.save(target_path + pdf_filename.replace(".pdf", ".png"), "PNG")    
 
 if __name__ == "__main__":
     pdf_path= "C:\\Users\\Hussein\\Desktop\\MicroBiology\\Wastewater Microbiology - Gabriel Bitton.pdf"
