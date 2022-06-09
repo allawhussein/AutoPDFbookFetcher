4b8339770ad5906095a9a4c3dafda369244ba97a
allawhussein <allawhussein@gmail.com>
2022-06-09 16:39:35 +0300

    added main code to csv_books_metadata_list.py the loop will collect all metadata and place them in a csv file

--- csv_books_metadata_list.py
+++ csv_books_metadata_list.py
@@ -12,3 +12,14 @@ from local_books_manager import details_fetcher_to_csv
 source_folder_path= "C:\\Users\\Hussein\\Desktop\\Microbiology\\"
 destination_folder_path= "C:\\Users\\Hussein\\Desktop\\Microbiology\\DONE-MicroBiology\\"
 image_folder_path= "C:\\Users\\Hussein\\Desktop\\Microbiology\\Images-Microbiolog\\"
+csv_path= "C:\\Users\\Hussein\\Desktop\\Microbiology\\Microbiology Metadata.csv"
+driver = webdriver.Chrome(service = ChromeService(executable_path=ChromeDriverManager().install()))
+
+book_paths= []
+for filename in os.listdir(source_folder_path):
+    if ".pdf" in filename:
+        book = []
+        image_path= pdf_cover_page_skimmer.create_cover_image(source_folder_path, image_folder_path, filename)
+        book_name, book_authors, publication_date, book_review = book_metadata_seeker.google_books(filename, driver)
+        book_path = source_folder_path + filename
+        details_fetcher_to_csv.csv_book_details(book_name, book_authors, book_review, publication_date, book_path, image_path, csv_path)
