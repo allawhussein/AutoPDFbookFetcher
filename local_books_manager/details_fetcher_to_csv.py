3306aa95db60a432a501c01d0c65308e5e33950e
allawhussein <allawhussein@gmail.com>
2022-06-17 08:23:59 +0300

    changed delimter for csv header writer

--- local_books_manager/details_fetcher_to_csv.py
+++ local_books_manager/details_fetcher_to_csv.py
@@ -3,11 +3,11 @@ import os
 def csv_book_details(book_name, book_authors, book_review, publication_date, book_path, image_path, csv_path):
     if not os.path.exists(csv_path):
         with open(csv_path, 'w') as csv_file:
-            csv_file.write("BookName, BookAuthor(s), BookReview, PublicatoinDate, BookPATH, ImagePATH\n")
+            csv_file.write("BookName|BookAuthor(s)|BookReview|PublicatoinDate|BookPATH|ImagePATH\n")
     else:
         with open(csv_path, 'r') as csv_file:
             header= csv_file.readline()
-        if header != "BookName, BookAuthor(s), BookReview, PublicatoinDate, BookPATH, ImagePATH\n":
+        if header != "BookName|BookAuthor(s)|BookReview|PublicatoinDate|BookPATH|ImagePATH\n":
             return csv_book_details(book_name, book_authors, book_review, publication_date, book_path, image_path, csv_path)
 
     with open(csv_path, 'a') as csv_file:
